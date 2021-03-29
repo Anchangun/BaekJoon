98930727d5064d42fa6d26e702d9b0fa87075ead
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-03-29 18:02:24 +0900

    Create : 11654. ASCII

---
+++ Question/String/11654. ASCII/cl_solution.h
@@ -0,0 +1,9 @@
+#pragma once
+class cl_solution
+{
+private: 
+	void fn_show();
+public :
+	void fn_run();
+};
+
---
+++ Question/String/11654. ASCII/quest.h
@@ -0,0 +1,18 @@
+#pragma once
+/*
+11654. ASCII
+
+Quest :
+문자, 숫자, 특수문자를 넣으면 아스키 코드를 알려주는 코드를 작성하시오.
+
+Input :
+A
+Output :
+65
+
+Input :
+C
+Ouput :
+67
+
+*/
