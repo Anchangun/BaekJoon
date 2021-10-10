222ed8fa8f8bfe3f50a0dee7d0f63d461df91bc3
DESKTOP-V7DNS3G\chang <changun516@gmail.com>
2021-10-10 18:54:40 +0900

    no message

---
+++ Question/Math/Calculation/10869. 사칙연산/cl_solution.h
@@ -0,0 +1,13 @@
+#pragma once
+#include<iostream>
+class cl_solution{
+	private :
+		int fn_add(int a, int b);
+		int fn_sub(int a, int b);
+		int fn_mul(int a, int b);
+		int fn_div(int a, int b);
+		int fn_rem(int a, int b);
+	public :
+		bool fn_run(int a, int b);
+};
+
