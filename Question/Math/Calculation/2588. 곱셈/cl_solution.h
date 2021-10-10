22427158d136ec07fc51cd2236e14e6b7f2c706e
DESKTOP-V7DNS3G\chang <changun516@gmail.com>
2021-10-10 19:57:31 +0900

    Update : erroe remove file

--- Question/Math/Calculation/2588. 곱셈/cl_solution.h
+++ Question/Math/Calculation/2588. 곱셈/cl_solution.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <iostream>
+<<<<<<< HEAD
+class cl_solution{
+	public :
+		void fn_run(int a, int b);
+};
+
+=======
 class cl_solution {
 public:
 	void fn_run(int a, int b);
 };
+>>>>>>> Math
--- Question/Math/Calculation/2588. 곱셈/main.cpp
+++ Question/Math/Calculation/2588. 곱셈/main.cpp
@@ -1,10 +1,19 @@
 #include"cl_solution.h"
 int main() {
+<<<<<<< HEAD
+		cl_solution* sol = new cl_solution();
+		int a = 0, b = 0;
+		std::cin >> a;
+		std::cin >> b;
+		sol->fn_run(a,b);
+		delete sol;
+=======
 	cl_solution* sol = new cl_solution();
 	int a = 0, b = 0;
 	std::cin >> a;
 	std::cin >> b;
 	sol->fn_run(a, b);
 	delete sol;
+>>>>>>> Math
 	return 0;
 }
