4eed8f18cb749a3d2a4451f5b8be963cd1e23b4d
DESKTOP-V7DNS3G\chang <changun516@gmail.com>
2021-10-19 23:44:04 +0900

    Create : 3052. 나머지

---
+++ Question/Array/3052. 나머지/cl_solution.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include<cstring>
+#define SIZE 10
+#define REM_NUM 42
+
+class cl_solution{
+private :
+	int* arr_data;
+public :
+	cl_solution();
+	int fn_run();
+};
+
