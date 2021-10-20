0d41e9b0e24c2b6428125d1fd6eed9c0baf5f527
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-10-21 05:47:13 +0900

    Create : 8958. OX퀴즈

---
+++ Question/Array/8958. OX퀴즈/cl_solution.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<iostream>
+#include<cstring>
+#include <cstdlib>
+class cl_solution{
+public:
+	void fn_run(int num);
+};
+
---
+++ Question/Array/8958. OX퀴즈/main.cpp
@@ -0,0 +1,9 @@
+#include"cl_solution.h"
+
+int main() {
+	cl_solution sol;
+	int num=0;
+	std::cin >> num;
+	sol.fn_run(num);
+	return 0;
+}
