40bec58a232b04a8939df28ec4e499b430776d03
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-10-24 00:34:08 +0900

    Create : 4344. 평균은 넘겠지

---
+++ Question/Array/4344. 평균은 넘겠지/cl_solution.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include<iostream>
+#include<cstring>
+class cl_solution{
+public :
+	void fn_run(int num);
+};
+
---
+++ Question/Array/4344. 평균은 넘겠지/main.cpp
@@ -0,0 +1,10 @@
+#include "cl_solution.hpp"
+
+int main() {
+	cl_solution sol;
+	int num = 0;
+	std::cin >> num;
+	sol.fn_run(num);
+	return 0;
+	
+}
