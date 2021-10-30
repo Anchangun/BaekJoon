a9b4641fe2c29c39bab18dff5eddcae05a1c7fb1
DESKTOP-V7DNS3G\chang <changun516@gmail.com>
2021-10-30 22:00:02 +0900

    Create : 1065. 한수

---
+++ Question/BruteForce/1065. 한수/cl_solution.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include<iostream>
+class cl_solution
+{
+	private :
+		bool fn_arithmetic_progression(int num);
+	public :
+		int fn_run(int num);
+};
+
---
+++ Question/BruteForce/1065. 한수/main.cpp
@@ -0,0 +1,10 @@
+#include"cl_solution.hpp"
+
+int main() {
+	int num = 0;
+	cl_solution sol;
+	std::cin >> num;
+	std::cout<<sol.fn_run(num);
+
+	return 0;
+}
