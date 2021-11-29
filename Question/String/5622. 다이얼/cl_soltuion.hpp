a686ffb077d020b3cb77eab05c842bd5b929d181
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-11-29 10:02:22 +0900

    Create : 5622. 다이얼

---
+++ Question/String/5622. 다이얼/cl_soltuion.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<string>
+class cl_soltuion{
+private :
+	std::vector<int> dial_num;
+public :
+
+	int fn_run(std::string user_data);
+};
+
