91ce396f06afe8d271ab64f9f1ac3113fc358b0f
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-12-06 14:03:44 +0900

    Create : 1316. 그룹 단어 체크

---
+++ Question/String/1316. 그룹 단어 체커/cl_solution.cpp
@@ -0,0 +1,12 @@
+#include "cl_solution.hpp"
+
+int cl_solution::fn_run(int num_size){
+    int num_lp = 0;
+    while (num_lp < num_size) {
+        std::string temp_msg;
+        std::cin >> temp_msg;
+        
+
+    }
+    return 0;
+}
