c3b4e4f21eab19cbaaf534796565188a14c99ac2
DESKTOP-V7DNS3G\chang <changun516@gmail.com>
2022-02-06 23:32:01 +0900

    Create : 5338. 마이크로소프트 로고

---
+++ Question/Print/5338. 마이크로소프트 로고/cl_solution.cpp
@@ -0,0 +1,12 @@
+#include "cl_solution.hpp"
+
+void cl_solution::fn_run()
+{
+	std::string result="       _.-;;-._\n";
+	result += "'-..-'|   ||   |\n";
+	result += "'-..-'|_.-;;-._|\n";
+	result += "'-..-'|   ||   |\n";
+	result += "'-..-'|_.-''-._|";
+
+	std::cout << result;
+}
