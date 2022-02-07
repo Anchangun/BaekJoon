9a1c5788bb3d3dd92554f732897da338680318a8
DESKTOP-V7DNS3G\chang <changun516@gmail.com>
2022-02-07 23:31:34 +0900

    Create : 5337. 웰컴

---
+++ Question/Print/5337. 웰컴/cl_solution.cpp
@@ -0,0 +1,10 @@
+#include "cl_solution.hpp"
+
+void cl_solution::fn_run()
+{
+	std::string result;
+	result = ".  .   .\n";
+	result += "|  | _ | _. _ ._ _  _\n";
+	result += "|/\\|(/.|(_.(_)[ | )(/.";
+	std::cout << result;
+}
