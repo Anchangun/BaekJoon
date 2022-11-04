1abddb2541c35ade4436099157f2d56afc56a801
안창언 <changun516@gmail.com>
2022-11-04 09:13:55 +0900

    25083. 새싹

---
+++ Question/Print/25083. 새싹/Solution.cpp
@@ -0,0 +1,12 @@
+#include "Solution.hpp"
+
+void Solution::run()
+{
+	std::cout <<"         ,r\'\"7"<< std::endl;
+	std::cout <<"r`-_   ,\'  ,\/"<< std::endl;
+	std::cout <<" \\. \". L_r\'"<< std::endl;
+	std::cout << "   \`~\\/" << std::endl;
+	std::cout << "      \|" << std::endl;
+	std::cout << "      \|" << std::endl;
+
+}
