0e36b300776abf39de8a80655adc8c25504b5250
DESKTOP-HHITDO3\Changun <changun516@gmail.com>
2023-04-28 16:01:56 +0900

    chore : file move

--- Question/Math/Calculation/14681. 사분면 고르기/cl_solution.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include<iostream>
-class cl_solution{
-public :
-	int fn_quadrant(int x, int y);
-};
-
--- Question/Print/10951. A+B - 4/main.cpp
+++
@@ -1,7 +0,0 @@
-#include"cl_solution.h"
-int main() {
-	cl_solution sol;
-	sol.fn_run();
-	return 0;
-
-}
